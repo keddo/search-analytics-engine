88f67ee560d6bb1c77815b4c85a70d8ca648498e
keddo <adu.kedir.abdurahman@gmail.com>
2024-03-28 07:50:39 +0300

    edit dev

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -2,9 +2,9 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << "real-time-search-analytics.up.railway.app"
   # Code is not reloaded between requests.
   config.enable_reloading = false
+  config.hosts << "real-time-search-analytics.up.railway.app"
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
